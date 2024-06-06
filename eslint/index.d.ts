ace7eb1c45f380b5e31e9cf4f07a6a04e3a26037
Wannes Salomé <11842931+waspeer@users.noreply.github.com>
2024-06-06 11:36:25 +0200

    fix ts types

--- eslint/index.d.ts
+++ eslint/index.d.ts
@@ -4,4 +4,5 @@ export const configs: {
   base: Linter.FlatConfig[];
   solid: Linter.FlatConfig[];
   react: Linter.FlatConfig[];
+  solidReact: (options: { solidDir: string; reactDir: string }) => Linter.FlatConfig[];
 };
