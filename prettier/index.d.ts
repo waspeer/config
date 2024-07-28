62e06303d8c0aa9ca99e3a6cc9544ba9ba5c8a46
Wannes Salomé <11842931+waspeer@users.noreply.github.com>
2024-07-28 21:38:54 +0200

    fix: ts types for custom prettier config

--- prettier/index.d.ts
+++ prettier/index.d.ts
@@ -1,3 +1,6 @@
 import type { Config } from 'prettier';
+import type { PartialDeep } from 'type-fest';
 
 export const config: Config;
+
+export function customConfig(partialConfig: PartialDeep<Config>): Config;
