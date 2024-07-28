00623b6adcc24fc2a1abfbb002627860865a6cca
Wannes Salomé <11842931+waspeer@users.noreply.github.com>
2024-07-28 21:37:22 +0200

    support custom prettier configs

--- prettier/index.js
+++ prettier/index.js
@@ -1,3 +1,5 @@
+import defaults from 'defaults';
+
 /** @type {import("prettier").Config} */
 export const config = {
   plugins: ['prettier-plugin-astro', 'prettier-plugin-css-order', 'prettier-plugin-packagejson'],
@@ -16,3 +18,8 @@ export const config = {
   singleQuote: true,
   trailingComma: 'all',
 };
+
+/** @type {(partialConfig: import('type-fest').PartialDeep<import("prettier").Config>) => import("prettier").Config} */
+export function customConfig(partialConfig) {
+  return defaults(partialConfig, config);
+}
